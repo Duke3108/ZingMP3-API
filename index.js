a808689b08109d4a570b1d3b253dd4d48e2caa33
Duke <tuoc31082004@gmail.com>
2024-10-20 19:52:37 +0700

    testdeloy

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ app.get("/", (req, res) => {
 const ZingMp3Router = require("./src/routes/ZingRouter")
 app.use("/api", cors({ origin: process.env.URL_CLIENT }), ZingMp3Router)
 
-const ZingMp3Router1 = require("./src/routes/ZingRouter")
-app.use("/api", cors({ origin: process.env.URL_CLIENT1 }), ZingMp3Router1)
 
 // Page Error
 app.get("*", (req, res) => {
